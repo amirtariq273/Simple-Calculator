95b534725d49a382871ba2f1b4131957a8777655
Amir Tariq <amirtariq273@gmail.com>
2024-03-22 22:02:43 +0500

    the addition in top last line  THE END

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operateas to perform operation",
         type: "list",
@@ -23,3 +23,4 @@ else if (answer.operater === "Multiplication") {
 else if (answer.operater === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
+console.log("THE END");
--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operateas to perform operation",
         type: "list",
@@ -23,5 +23,7 @@ if (answer.operater === "Addition") {
 } else if (answer.operater === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
+console.log("THE END");
+
 
 
